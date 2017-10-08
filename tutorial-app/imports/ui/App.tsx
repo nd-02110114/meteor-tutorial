21b876f10d602a4a5824ad1d73ac888f67a81e55
nd-02110114 <nd.12021218@gmail.com>
2017-10-09 00:04:26 +0900

    finish forms and events

--- tutorial-app/imports/ui/App.tsx
+++ tutorial-app/imports/ui/App.tsx
@@ -1,16 +1,32 @@
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
  
 import Task from './Task';
 import { Tasks } from '../api/tasks';
 
-export interface TasksPropType {
+interface TasksPropType {
  tasks: { _id: number; text: string; }[];
 }
  
 // App component - represents the whole app
 class App extends React.Component<TasksPropType,{}> {
-  renderTasks() {
+  private textInput: HTMLInputElement;
+
+  handleSubmit = (event: any):void  => {
+    event.preventDefault();
+    // Find the text field via the React ref
+    const text = this.textInput.value.trim();
+    // insert Database
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+    // Clear form
+    this.textInput.value = '';
+  }
+
+  renderTasks = () => {
     return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
@@ -21,6 +37,13 @@ class App extends React.Component<TasksPropType,{}> {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+          <form className="new-task" onSubmit={this.handleSubmit} >
+            <input
+              type="text"
+              ref={(ref) => this.textInput = ref}
+              placeholder="Type to add new tasks"
+            />
+          </form>
         </header>
 
         <ul>
@@ -33,6 +56,6 @@ class App extends React.Component<TasksPropType,{}> {
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   };
 }, App);
