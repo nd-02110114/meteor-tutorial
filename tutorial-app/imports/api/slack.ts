bdf7ba9022b9c8f44c4eb051dc116a7bdf1c8d84
nd-02110114 <nd.12021218@gmail.com>
2017-10-26 16:05:51 +0900

    change code place

--- tutorial-app/imports/api/slack.ts
+++ tutorial-app/imports/api/slack.ts
@@ -73,52 +73,7 @@ async function getAllChannelInfo() {
   }
 };
 
-// draw d3 graph
-function drawBarGraph () {
-  const a = ["test", "hoge", "hello d3"]
-  const alldata = getAllChannelInfo();
-
-  alldata.then(e => {
-    Promise.all(e).then((d3Data:d3DataType[]) => {
-      // arrrange data
-      return formatData(d3Data);
-    }).then((d3FilterData: formatDataType[]):void => {
-      console.log(d3FilterData);
-      Template.body.onRendered(function () {
-        d3BarDraw(d3FilterData);
-      });
-    });
-  });
-};
-
-// Draw Bar Graph
-drawBarGraph();
-
-// draw d3 bar graph  
-function d3BarDraw (d3Data) {
-  nv.addGraph(function() {
-    var chart = nv.models.multiBarHorizontalChart()
-        .x(function(d) { return d.label })
-        .y(function(d) { return d.value })
-        .margin({top: 30, right: 20, bottom: 50, left: 175})
-        .showValues(true)           //Show bar value next to each bar.
-        .tooltips(true)             //Show tooltips on hover.
-        .transitionDuration(350)
-        .showControls(true);        //Allow user to switch between "Grouped" and "Stacked" mode.
-
-    chart.yAxis
-        .tickFormat(d3.format(',.2f'));
-
-    d3.select('svg')
-        .datum(d3Data)
-        .call(chart);
-
-    nv.utils.windowResize(chart.update);
-
-    return chart;
-  });
-}
-
+// formatting
 function formatData(data) {
   const filterData = data.filter(function(element){
     return (element.message > 15);
@@ -138,3 +93,49 @@ function formatData(data) {
 
   return formatData;
 }
+
+// draw d3 bar graph  
+function d3BarDraw (d3Data) {
+  nv.addGraph(function() {
+    var chart = nv.models.multiBarHorizontalChart()
+        .x(function(d) { return d.label })
+        .y(function(d) { return d.value })
+        .margin({top: 30, right: 20, bottom: 50, left: 175})
+        .showValues(true)           //Show bar value next to each bar.
+        .tooltips(true)             //Show tooltips on hover.
+        // .transitionDuration(350)
+        .showControls(true);        //Allow user to switch between "Grouped" and "Stacked" mode.
+
+    chart.yAxis
+        .tickFormat(d3.format(',.2f'));
+
+    d3.select('svg')
+        .datum(d3Data)
+        .call(chart);
+
+    nv.utils.windowResize(chart.update);
+
+    return chart;
+  });
+}
+
+// draw d3 graph
+function drawBarGraph () {
+  const a = ["test", "hoge", "hello d3"]
+  const alldata = getAllChannelInfo();
+
+  alldata.then(e => {
+    Promise.all(e).then((d3Data:d3DataType[]) => {
+      // arrrange data
+      return formatData(d3Data);
+    }).then((d3FilterData: formatDataType[]):void => {
+      // console.log(d3FilterData);
+      Template.body.onRendered(function () {
+        d3BarDraw(d3FilterData);
+      });
+    });
+  });
+};
+
+// Draw Bar Graph
+drawBarGraph();
