5e714910517cf99ee1e90b119c45261c323ee994
nd-02110114 <nd.12021218@gmail.com>
2017-10-26 21:23:41 +0900

    finish d3 graph

--- tutorial-app/imports/api/slack.ts
+++ tutorial-app/imports/api/slack.ts
@@ -98,17 +98,13 @@ function formatData(data) {
 // draw d3 bar graph  
 function d3BarDraw (d3Data) {
   nv.addGraph(function() {
-    var chart = nv.models.multiBarHorizontalChart()
+    const chart = nv.models.multiBarHorizontalChart()
         .x(function(d) { return d.label })
         .y(function(d) { return d.value })
-        .margin({top: 30, right: 20, bottom: 50, left: 175})
-        .showValues(true)           //Show bar value next to each bar.
-        .tooltips(true)             //Show tooltips on hover.
-        // .transitionDuration(350)
-        .showControls(true);        //Allow user to switch between "Grouped" and "Stacked" mode.
-
+        .margin({top: 30, right: 20, bottom: 50, left: 175});
+     
     chart.yAxis
-        .tickFormat(d3.format(',.2f'));
+        .tickFormat(d3.format("01d"));
 
     d3.select('svg')
         .datum(d3Data)
@@ -129,10 +125,8 @@ function drawBarGraph () {
       // arrrange data
       return formatData(d3Data);
     }).then((d3FilterData: formatDataType[]):void => {
-      // console.log(d3FilterData);
-      Template.body.onRendered(function () {
-        d3BarDraw(d3FilterData);
-      });
+      // draw nvd3
+      d3BarDraw(d3FilterData);
     });
   });
 };
